678c2af404827179ca63cf0fdf0ca53fb4d9a93d
Alpha7ero <adahmo2003@gmail.com>
2024-02-11 22:38:09 +0200

    m

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,12 @@ module.exports = {
             assert: require.resolve('assert/'),
             net: require.resolve('net-browserify'),
             tls: require.resolve('tls-browserify'),
-            assert: require.resolve('assert')
+            assert: require.resolve('assert'),
+            zlib: require.resolve('browserify-zlib'),
+            querystring: require.resolve('querystring-es3'),
+            crypto: require.resolve('crypto-browserify'),
+            stream: require.resolve('stream-browserify'),
+            fs: require.resolve('fs')
         }
     }    
 };
